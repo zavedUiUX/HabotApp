625f19cceb43aa9b4b252e780fc011d8a72584ee
zavedUiUX <uiux6@habot.io>
2024-12-30 20:26:58 +0530

    first

--- App.js
+++ App.js
@@ -1,12 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from "react";
+import { StyleSheet, View, StatusBar } from "react-native";
+import { WebView } from "react-native-webview";
+import { preventAutoHideAsync, hideAsync } from "expo-splash-screen"; // Only import necessary functions
 
 export default function App() {
+  const [isLoading, setIsLoading] = useState(true); // State to track if the web page is loading
+
+  useEffect(() => {
+    // Prevent the splash screen from hiding automatically
+    preventAutoHideAsync();
+  }, []);
+
+  const handleWebViewLoad = () => {
+    // WebView is fully loaded, now hide the splash screen
+    setIsLoading(false);
+    hideAsync();
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar barStyle="light-content" backgroundColor="#263238" />
+      {/* WebView component that loads the URL */}
+      <WebView
+        source={{ uri: "https://culturebuilding.habot.io/" }}
+        onLoad={handleWebViewLoad} // Calls handleWebViewLoad when WebView is loaded
+      />
     </View>
   );
 }
@@ -14,8 +32,5 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
